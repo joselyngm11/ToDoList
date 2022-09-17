893c1d1de85f8d39054c137f085917bc17d23317
joselyngm11 <joselyngm.47@gmail.com>
2022-09-17 18:27:21 +0000

    List group

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = (props)  => {
+
+	const [inputValue, setInputValue] = useState("")
+	const [cards, setCards] = useState([])
+
+	const handleAdd = (e) => {
+		let newArray = [...cards]
+		//if(inputValue.length > 3){
+		if (e.key === 'Enter'){
+			newArray.push(inputValue)
+			setCards(newArray)
+			setInputValue("")
+		}
+			
+		//}else{
+			//alert("NO SEAS VAGO ESCRIBE MAS")
+		//}
+		/*setCards([...cards, inputValue])*/
+	}
+
+	// const _handleKeyDown = (e) => {
+	// 	if (e.key === 'Enter') {
+	// 	  handleAdd
+	// 	}
+	// }
+	
+  	return (
+		<div className="row d-flex justify-content-center">
+			<div className="col">
+				<input value={inputValue} 
+				onChange={(event) =>setInputValue(event.target.value)} 
+				type="text"
+				onKeyDown={handleAdd}></input>
+				{/* <button onClick={handleAdd}> anadir </button> */}
+			</div>
+			<ListGroup className="col-12">
+				{
+					cards.map((card,index)=>{
+						return(
+							// <Card key={index} style={{ width: '18rem' }}>
+							// 	<Card.Img variant="top" src="https://picsum.photos/200/300" />
+							// 	<Card.Body>
+							// 		<Card.Title>{card}</Card.Title>
+							// 		<Card.Text>
+							// 		Some quick example text to build on the card title and make up the
+							// 		bulk of the card's content.
+							// 		</Card.Text>
+							// 		<Button variant="primary">Go somewhere</Button>
+							// 	</Card.Body>
+							// </Card>
+							<ListGroup.Item key={index}>{card}</ListGroup.Item>
+						)
+					})
+					
+				}
+			</ListGroup>
 		</div>
-	);
-};
+  	);
+}
 
 export default Home;
